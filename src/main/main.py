c4397e51992c86c992947eeeaf7bebbe71ba83e2
Mat1020 <matias.tajan@gmail.com>
2026-04-07 16:35:24 -0400

    Auto Clicker runable on Tkinter

--- src/main/main.py
+++ src/main/main.py
@@ -11,22 +11,24 @@ def pause_and_resume_auto_clicker(start, root, frame):
     elif start == True:
         ttk.Label(frame, text="Started.").grid(column=0, row=0)
 
+def init_tkinter(root, button):
+    button.pack()
+    root.mainloop()
 
 def main():
     root = Tk()
     root.title("Auto Clicker")
     root.geometry(f"{WIDTH}x{HEIGHT}")
-    frame_window = ttk.Frame(root, padding=10)
-    frame_window.grid()
+    # frame_window = ttk.Frame(root, padding=10)
+    # frame_window.grid()
 
-    start_auto_clicker = BooleanVar()
+    # start_auto_clicker = BooleanVar()
     # start_auto_clicker.set(False)
-    print(start_auto_clicker)
     # pause_and_resume_auto_clicker(start_auto_clicker, root, frame_window)
+    button = Button(root, text="Start", command=toggle_key.main)
 
 
-    root.mainloop()
-    # toggle_key.main()
+    init_tkinter(root, button)
 
 if __name__ == "__main__":
     main()
